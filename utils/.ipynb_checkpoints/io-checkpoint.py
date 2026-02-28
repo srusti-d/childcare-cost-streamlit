1c573f9e4bf7ffd6d9bc7a608d8a3a2bb7250414
srusti-d <srustidonapati@gmail.com>
2026-02-28 16:44:37 -0600

    add all streamlit dirs and files

---
+++ utils/.ipynb_checkpoints/io-checkpoint.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+from vega_datasets import data
+
+@st.cache_data
+def load_weather() -> pd.DataFrame:
+    df = data.seattle_weather()
+    df["date"] = pd.to_datetime(df["date"])
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
+    df["month_name"] = df["date"].dt.strftime("%b")
+    return df
