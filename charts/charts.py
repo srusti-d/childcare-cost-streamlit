ee923dacbdde50b8af07bd52729e01d56c1c1af7
srusti-d <srustidonapati@gmail.com>
2026-03-03 23:39:05 -0600

    ../charts/__init__.py

--- charts/charts.py
+++ charts/charts.py
@@ -2,7 +2,7 @@ import altair as alt
 import pandas as pd
 import geopandas as gpd
 
-from utils import data_io as io
+from utils.data_io import normalize_features_to_unit_box
 
 def base_theme():
     return {
